b2a0847c49a57d5a578709f1a862cb4d3dc22457
Manish kumar <59960810+manish1822510059@users.noreply.github.com>
2020-07-31 23:55:05 +0530

    Add files via upload
    ***********Game :-Snake,Water,Gun************

---
+++ Project2_snake_water_gun_game.py
@@ -0,0 +1,67 @@
+import random
+list1=["snake","water","gun"]
+
+value=random.choice(list1)
+print("******Snake Water Gun Game******")
+name=input("Please Enter your name: ")
+cu=0
+cp=0
+c=5
+while c>0:
+    a = input("You Choice 1. for Snake 2. for Water 3.for Gun\n") 
+    if  a == "snake":
+        
+        if a == value:
+            print(f"Match is draw::{a} vs {value} ")
+            c=c-1
+            print("Chance left",c)
+        elif value == "water":
+            print(f"You won ::{a} vs {value}")
+            cu+=1
+            c=c-1
+            print("Chance left",c)
+        elif value == "gun":
+            print(f"You Lost ::{a} vs {value}")
+            cp+=1
+            c=c-1
+            print("Chance left",c) 
+    elif a=="water":
+        if a == value:
+            print(f"Match is draw::{a} vs {value} ")
+            c=c-1
+            print("Chance left",c)
+        elif value == "gun":
+            print(f"You won ::{a} vs {value}")
+            cu+=1
+            c=c-1
+            print("Chance left",c)
+        elif value == "water":
+            print(f"You Lost ::{a} vs {value}")
+            cp+=1
+            c=c-1
+            print("Chance left",c)
+     
+    elif a=="gun":
+        if a == value:
+            print(f"Match is draw::{a} vs {value} ")
+            c=c-1
+            print("Chance left",c)
+        elif value == "water":
+            print(f"You won ::{a} vs {value}")
+            cu+=1
+            c=c-1
+            print("Chance left",c)
+        elif value == "snake":
+            print(f"You Lost ::{a} vs {value}")
+            cp+=1
+            c=c-1
+            print("Chance left",c)
+        
+if(cu>cp):
+    print(f"\c{name} is the Winner Your score:\t".upper(),cu)
+else:
+    print("\nSorry Computer is a winner Computer score:\t".upper(),cp)
+
+         
+         
+
